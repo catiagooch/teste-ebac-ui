d1892efe29c480cf16f3107f8bf54b3f56c4f67c
Catia Gooch <catiagooch@gmail.com>
2024-04-09 16:18:31 +0100

    Primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: login', () => {
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('catia.lisboa@teste.com.br')
+        cy.get('#password').type('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, catia.lisboa')
+    })
+})
