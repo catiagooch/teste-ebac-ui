fd83004dd2ee0b5232ba5c8525d39d15a916f8f1
Catia Gooch <catiagooch@gmail.com>
2024-04-11 13:12:04 +0100

    atualização com comando customizado

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -6,3 +6,18 @@ Cypress.Commands.add('login', (usuario, senha) => {
 
 
  })
+
+Cypress.Commands.add('preCadastro'), (email, senha, nome, sobrenome) => {
+    cy.get('#reg_email').type(email)
+    cy.get('#reg_password').type(senha)
+    cy.get(':nth-child(4) > .button').click()
+
+    cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+    cy.get('#account_first_name').type(nome)
+    cy.get('#account_last_name').type(sobrenome)
+    cy.get('.woocommerce-Button').click()
+
+}
+
+
+ 
